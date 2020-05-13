94cebafd660f2972bd0daf1463d86eb4a79dd6b0
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-13 21:35:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,29 +1,3 @@
 def third_challenge
-  epic_tragedy = {
-   :montague => {
-      :patriarch => 
-        {name: "Lord Montague", age: "53"
-        },
-      :matriarch => {name: "Lady Montague", age "54"
-        },
-      :hero => {name: "Romeo", age: "15"
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
-  }
 
-  
 end
